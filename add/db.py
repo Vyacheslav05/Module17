7d70bcecda18db1b93ca37bd7aedbf94db4e9164
Vyacheslav05 <vyacheslav.lobanov@wellpack.org>
2024-11-11 15:44:52 +0300

    Add files via upload

---
+++ add/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DATABASE_URL = 'sqlite:///taskmanager.db'
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
